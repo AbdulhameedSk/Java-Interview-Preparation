04eac9699e3526bb8b782a437990f7d7e3222ae3
ABDULHAMEED <skshaikabdulhameed786@gmail.com>
2024-01-31 18:55:53 +0530

    Fixed Warnings

--- 5.Objects/TestUserLogin.java
+++ 5.Objects/TestUserLogin.java
@@ -1,6 +1,6 @@
 public class TestUserLogin {
     public static void main(String[] args) {
-        UserService userService =new UserService();
+        new UserService();
         Login login=UserService.authenticate("wally@gmail.com","abcdefg");
         User user=(User) login;
         System.out.println("Hello, "+user.getName());
